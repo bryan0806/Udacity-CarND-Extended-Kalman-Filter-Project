11ff2b8d35346e0e789073f3b08bb34373b12037
Chunwei Liu(bryan liu) <chunwei.liu@gmail.com>
2017-07-18 19:09:43 +0800

    Finished Laser update predict functions , compiling ok, but run failed on Update function ......... need debug!

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -88,6 +88,37 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
     }
 
+    // Call KalmanFilter::Init to setup x_in, P_in, F_in,H_in, R_in, Q_in
+
+    //state covariance matrix P
+    ekf_.P_ = MatrixXd(4, 4);
+    ekf_.P_ << 1, 0, 0, 0,
+              0, 1, 0, 0,
+              0, 0, 1000, 0,
+              0, 0, 0, 1000;
+
+
+    //measurement covariance
+    //ekf_.R_ = MatrixXd(2, 2);
+    //ekf_.R_ << 0.0225, 0,
+    //          0, 0.0225;
+
+    //measurement matrix
+    //kf_.H_ = MatrixXd(2, 4);
+    //kf_.H_ << 1, 0, 0, 0,
+    //                  0, 1, 0, 0;
+
+    //the initial transition matrix F_
+    ekf_.F_ = MatrixXd(4, 4);
+    ekf_.F_ << 1, 0, 1, 0,
+               0, 1, 0, 1,
+               0, 0, 1, 0,
+               0, 0, 0, 1;
+
+    //set the acceleration noise components
+    //noise_ax = 5;
+    //noise_ay = 5;
+
     // done initializing, no need to predict or update
     is_initialized_ = true;
     return;
@@ -99,14 +130,38 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   /**
    TODO:
-     * Update the state transition matrix F according to the new elapsed time.
+     * Update the                                                                                                                                                                                                                                                                                                                                                                                                                                                              state transition matrix F according to the new elapsed time.
       - Time is measured in seconds.
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
 
-  ekf_.Predict();
+  //compute the time elapsed between the current and previous measurements
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+  previous_timestamp_ = measurement_pack.timestamp_;
 
+  // TODO: YOUR CODE HERE
+  //1. Modify the F matrix so that the time is integrated
+  //cout << "F is "<< endl << kf_.F_ << endl;
+  ekf_.F_(0,2) = dt;
+  ekf_.F_(1,3) = dt;
+
+  //2. Set the process covariance matrix Q
+
+  //acceleration noise components
+  float noise_ax = 9;
+  float noise_ay = 9;
+
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ <<  pow(dt,4)/4*noise_ax,0,pow(dt,3)/2*noise_ax,0,
+             0,pow(dt,4)/4*noise_ay,0,pow(dt,3)/2*noise_ay,
+             pow(dt,3)/2*noise_ax,0,pow(dt,2)*noise_ax,0,
+             0,pow(dt,3)/2*noise_ay,0,pow(dt,2)*noise_ay;
+  //cout << "Q is now " << kf_.Q_ << endl;
+  //3. Call the Kalman Filter predict() function
+  //cout << "before predict" << endl;
+  ekf_.Predict();
+  //cout << "after predict" << endl;
   /*****************************************************************************
    *  Update
    ****************************************************************************/
@@ -117,10 +172,18 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the state and covariance matrices.
    */
 
+
+
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
   } else {
     // Laser updates
+    cout << "inside laser updates" << endl;
+    VectorXd z = VectorXd(2);
+    float x_measurement = measurement_pack.raw_measurements_[0];
+    float y_measurement = measurement_pack.raw_measurements_[1];
+    z << x_measurement,y_measurement;
+    ekf_.Update(z);
   }
 
   // print the output
